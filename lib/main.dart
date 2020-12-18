828f5c5f914c507dd44290559568b802a8086201
Bernardo Duarte <bernardo.silva@infobase.com.br>
2020-12-17 23:16:44 -0300

    add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-main() {
-  runApp(new QuizApp());
-}
+main() => runApp(QuizApp());
 
 class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Quiz'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('QuizApp'),
+        ),
+        body: Text('Question'),
+      ),
     );
   }
 }
