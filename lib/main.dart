3d10f3e24be5ae7059c9c5410516335d1a57af87
Bernardo Duarte <bernardo.silva@infobase.com.br>
2020-12-17 23:48:32 -0300

    usando Column children com RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,50 @@ main() => runApp(QuizApp());
 class QuizApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('QuizApp'),
         ),
-        body: Text('Question'),
+        body: Column(
+          children: [
+            Text(perguntas.elementAt(1)),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: () {
+                print('Pergunta 1 respondida!');
+              },
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: resposta2,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: resposta3Function(),
+            ),
+            RaisedButton(
+              child: Text('Resposta 4'),
+              onPressed: () => print('Pergunta 4 respondida!'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void resposta2() {
+    print('Pergunta 2 respondida!');
+  }
+
+  void Function() resposta3Function() {
+    return () {
+      print('Pergunta 3 respondida!');
+    };
   }
 }
