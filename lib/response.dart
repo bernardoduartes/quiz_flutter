449bbee9a359937893d35db955bce4ba70aa3d7e
Bernardo Duarte <bernardo.silva@infobase.com.br>
2020-12-21 20:38:26 -0300

    create response.dart

---
+++ lib/response.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class Response extends StatelessWidget {
+  final String text;
+
+  Response(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
